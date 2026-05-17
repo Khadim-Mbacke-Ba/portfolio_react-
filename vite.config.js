19f6382cdd772b8875dba29d2d2a67afbf01b45d
serigne khadim mbacke ba <serignekhadimmbackeba40@gmail.com>
2026-05-17 12:53:50 +0200

    fix vite config base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// ⚠️  Remplace 'mon-portfolio' par le nom EXACT de ton repo GitHub
 export default defineConfig({
   plugins: [react()],
-  base: '/mon-portfolio/',
+  base: '/portfolio_react-/',
 })
